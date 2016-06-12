acce68c832cb58871bba7b2b18c579352340f7eb
CodingPat <CodingPat@users.noreply.github.com>
2016-06-12 20:25:53 +0200

    push_01.rom

---
+++ roms/push_01.asm
@@ -0,0 +1,25 @@
+# push_01.rom
+# test push registers + cache registers 0-3 + memory adress
+
+#stack at 00FF
+MOVI SPL,FF
+MOVI SPH,00
+
+#push regs
+
+PUSH A
+PUSH B
+PUSH H
+PUSH L
+PUSH SPH
+PUSH SPL
+PUSH F
+PUSH REG0
+PUSH REG1
+PUSH REG2
+PUSH REG3
+PUSH M,0004
+
+#END
+HLT
+
